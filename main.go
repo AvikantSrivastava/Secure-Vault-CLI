4c05435013024c7fab14c0e7e786c64ba548eebd
Avikant Srivastava <asdavikant@gmail.com>
2021-09-12 01:35:20 +0530

    added login and signup handler

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
 type Color string
@@ -21,4 +23,34 @@ func colorize(color Color, message string) {
 
 func main() {
 	colorize(ColorYellow, "Welcome to Secret Vault")
+
+	loginCmd := flag.NewFlagSet("login", flag.ExitOnError)
+	signupCmd := flag.NewFlagSet("signup", flag.ExitOnError)
+
+	if len(os.Args) < 2 {
+		colorize(ColorRed, "Enter a valid command 'login' or 'signup")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "login":
+		fmt.Printf("login")
+		HandleLogin(loginCmd)
+	case "signup":
+		fmt.Printf("signup")
+		HandleLogin(signupCmd)
+
+	default: // if we don't understand the input
+	}
+
+}
+
+func HandleLogin(loginCmd *flag.FlagSet) {
+	loginCmd.Parse(os.Args[2:])
+
+}
+
+func HandleSignup(signupCmd *flag.FlagSet) {
+	signupCmd.Parse(os.Args[2:])
+
 }
